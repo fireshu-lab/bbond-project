ded5c68ca3139515abff26ee88067e0c12da59ed
fireshu <fireshu@gmail.com>
2026-03-09 13:36:44 +0000

    Upload project to GitHub

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Add JS here
+const generateBtn = document.getElementById('generate-btn');
+const numberElements = document.querySelectorAll('.number');
+
+generateBtn.addEventListener('click', () => {
+    const lottoNumbers = new Set();
+    while (lottoNumbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        lottoNumbers.add(randomNumber);
+    }
+
+    const sortedNumbers = Array.from(lottoNumbers).sort((a, b) => a - b);
+
+    numberElements.forEach((element, index) => {
+        element.textContent = sortedNumbers[index];
+    });
+});
